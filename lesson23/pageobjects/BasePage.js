5cb0823d25cd3a81725c7a1af0614cacfeaa8d13
Ylia Darafeichyk <ydarafeichyk@mail.ru>
2022-12-04 16:18:07 +0300

    Cucumber

---
+++ lesson23/pageobjects/BasePage.js
@@ -0,0 +1,6 @@
+class BasePage {
+
+}
+
+
+module.exports = { BasePage };
---
+++ lesson23/pageobjects/SearchResultPage.js
@@ -0,0 +1,11 @@
+const { BasePage } = require('./BasePage');
+
+class SearchResultPage extends BasePage {
+
+    get 'Search Titles'(){
+        return $('//h1');
+    }
+}
+
+module.exports = { SearchResultPage };
+
